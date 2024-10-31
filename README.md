e46c05c87fc100a17f362ef04c1ec0c55cbb4aed
Name <email@email.com>
2024-10-31 20:48:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Discovery (ARP Device Discovery Tool
+Discovery is a device discovery tool that operates via ARP packets that are transmitted across the network. It is quick to deploy and automatically determines which network interfaces you wish to use as well as calculates your subnet mask and its corresponding CIDR notation.
+# Application Usage
+## Requirements:
+- Scapy
+- JSON
+```
+python -m pip install ./requirements.txt
+```
+## Summary:
+This is an active reconnaissance tool and therefore it can be easily detected on networks that have active monitoring by systems such as an IDS or an administrator. If this tool is required for stealth usage, you should **NOT** use it as it uses broadcast frames which transmit across the entire network until it reaches its destination.
---
+++ discovery.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+# All software written by Tomas. (https://github.com/shelbenheimer/ata-shell)
+
+from scapy.all import ARP, srp, Ether, get_if_addr, conf
+from json import load
+import time
+import sys
+import os
+
+BANNER      = "Software written by Tomas. Available on GitHub. (https://github.com/shelbenheimer/ata-shell)"
+VENDOR_PATH = "Resources/manuf.json"
+
+class Discovery:
+	def __init__(self):
+		self.target  = self.FormatAddress(get_if_addr(conf.iface))
+		self.path    = f'{os.path.dirname(os.path.abspath(__file__))}/{VENDOR_PATH}'
+		self.vendors = self.PopulateVendors()
+
+	def GetHosts(self):
+		packet  = Ether(dst="FF:FF:FF:FF:FF:FF") / ARP(pdst=self.target)
+		replies = srp(packet, timeout=1, verbose=False)[0]
+
+		if not replies:
+			return []
+
+		hosts = []
+		for reply in range(0, len(replies)):
+			information = (replies[reply][1].psrc, replies[reply][1].hwsrc)
+			hosts.append(information)
+		return hosts
+
+	def FormatAddress(self, ip):
+		mask = "255.255.255.0"
+
+		split_mask = mask.split('.')
+		split_addr = ip.split('.')
+		counted = 0
+		for octet in range(0, len(split_mask)):
+			if split_mask[octet] == "0":
+				split_addr[octet] = "0"
+
+			binary = bin(int(split_mask[octet]))[2:]
+			for digit in binary:
+				if digit == '1': counted += 1
+
+		result = f'{".".join(split_addr)}/{counted}'
+		return result
+
+	def PopulateVendors(self):
+		try:
+			with open(self.path, 'r', encoding="utf8") as file:
+				vendors = load(file)
+				return vendors
+		except Exception as error:
+			print(error)
+			return {}
+
+	def FormatOUI(self, address):
+		formatted = address[:8].replace("-", ":")
+		return formatted
+
+	def GetVendor(self, address):
+		if not self.vendors:
+			return "Unobtainable"
+
+		try:
+			formatted = self.FormatOUI(address).upper()
+			return self.vendors[formatted]
+		except:
+			return "Unknown Vendor"
+
+def Main():
+	try:
+		discovery = Discovery()
+
+		print(BANNER)
+		if not discovery.PopulateVendors():
+			print("Failed to populate manufacturer database.")
+
+		print(f'Commencing scan on {discovery.target} on {time.ctime()}.')
+		start_time = time.time()
+		hosts = discovery.GetHosts()
+		if not hosts:
+			print("There was an error whilst attempting to scan the network.")
+			return
+
+		for host in hosts:
+			vendor = discovery.GetVendor(host[1])
+			print(f'{host[0]:<18} {host[1]:<20} {vendor:<20}')
+
+		elapsed = time.time() - start_time
+		formatted_elapsed = "".join(list(str(elapsed))[:5])
+		print(f'Finished scan in {formatted_elapsed}s.')
+	except Exception as error:
+		print(error)
+		return
+
+if __name__ == "__main__": Main()
